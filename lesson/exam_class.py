dc25b151da340c51fbc666dcd9e89d868c89b0ac
Mark <smailsist766@gmail.com>
2023-11-23 18:32:11 -0500

    save 1

--- lesson/exam_class.py
+++ lesson/exam_class.py
@@ -1,39 +1,43 @@
 import csv
+import logging
 
-#print('Здравствуй пользователь, тебя приветствует система учета студентов колледжа имени Яны Сергеевны Щавель\n')
+# print('Здравствуй пользователь, тебя приветствует система учета студентов колледжа имени Яны Сергеевны Щавель\n')
+
+
 
 class Student:
-    def __init__(self,student_name, student_age, student_course):
+    def __init__(self, student_name, student_age, student_course):
         self.student_name = student_name
         self.student_age = student_age
         self.student_course = student_course
         self.list_of_passed_exams = []
         self.gpa_student = 0
-    def add_passed_exames(self,grade):
+        logging.info(f"Добавили студента:{self.student_name}, {self.student_age} лет на курс {self.student_course}.")
+
+
+    def add_passed_exames(self, grade):
         self.list_of_passed_exams.append(grade)
         self.gpa_student = sum(self.list_of_passed_exams) / len(self.list_of_passed_exams)
         return self.gpa_student
-
-
 
     # 5. Напишите методы для расчета возможности перевода студента на следующий курс,
     # а также для добавления и удаления экзаменов у студента.
     def transferring_a_student_to_the_next_course(self):
         if len(self.list_of_passed_exams) > 5 and self.gpa_student > 3:
             self.student_course += 1
-            return self.student_name,' перешел на ',self.student_course,' курс.'
+            return self.student_name, ' перешел на ', self.student_course, ' курс.'
         elif len(self.list_of_passed_exams) < 5 or self.gpa_student < 3:
             return 'студент пока не может перейти на следующий курс'
-
-
 
     def scholarship(self):
         return 'У студента: ' + self.student_name + ' степендия ' + str((self.gpa_student * 0.2) * 2000)
 
     def del_examen(self):
         self.list_of_passed_exams.pop()
+
     def course_information(self):
-        return 'ученик '+self.student_name+' на ' +  str(self.student_course) + ' курсе'
+        return 'ученик ' + self.student_name + ' на ' + str(self.student_course) + ' курсе'
+
     def GPA_for_return(self):
         return 'У ученика: ' + self.student_name + ' средний балл ' + str(self.gpa_student)
 
@@ -49,7 +53,7 @@ class Student:
                 print(i)
                 x += float(i[3])
 
-        print('Средний балл всех студентов',x / p)
+        print('Средний балл всех студентов', x / p)
 
 
 
@@ -57,10 +61,10 @@ class University:
     def __init__(self):
         self.students = []
 
-    def add_student(self,student):
+    def add_student(self, student):
         self.students.append(student)
 
-    def dell_student(self,student):
+    def dell_student(self, student):
         self.students.remove(student)
 
     def total_gpa_students(self):
@@ -71,9 +75,24 @@ class University:
         # открытие файла в формате записи и юникода UTF-8
         with open('student_csv_file.csv', 'w', encoding='UTF-8', newline='') as f:
             writer = csv.writer(f)
-            #writer.writerow(['Имя','возраст','курс','средний балл'])
+            # writer.writerow(['Имя','возраст','курс','средний балл'])
             for student in self.students:
-                writer.writerow([student.student_name,student.student_age,student.student_course,student.gpa_student])
+                writer.writerow(
+                    [student.student_name, student.student_age, student.student_course, student.gpa_student])
+
+
+
+
+@logging
+def log(*args,**kwargs):
+    logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
+                        format="%(asctime)s %(levelname)s %(message)s", encoding='UTF-8')
+    logging.debug("A DEBUG Message")
+    logging.info("An INFO")
+    logging.warning("A WARNING")
+    logging.error("An ERROR")
+    logging.critical("A message of CRITICAL severity")
+
 
 
 student_1 = Student('Mark', '22', 1)
@@ -84,7 +103,6 @@ student_1.add_passed_exames(4)
 student_1.add_passed_exames(5)
 student_1.add_passed_exames(3)
 
-
 student_2.add_passed_exames(2)
 student_2.add_passed_exames(4)
 student_2.add_passed_exames(3)
@@ -93,7 +111,6 @@ student_3.add_passed_exames(5)
 student_3.add_passed_exames(5)
 student_3.add_passed_exames(5)
 
-
 mgsiit = University()
 mgsiit.add_student(student_1)
 mgsiit.add_student(student_2)
@@ -101,15 +118,14 @@ mgsiit.add_student(student_3)
 
 mgsiit.student_save_file()
 
-l = student_1.transferring_a_student_to_the_next_course()
-print(l)
+student_1.transferring_a_student_to_the_next_course()
+
+
+print(student_1.scholarship())
 # средний балл всех студентов
 # Student.overall_gpa()
-
-
 
 
 # 4. Создайте функции для сохранения информации о студентах в файле, а также для загрузки этой информации из файла.
 
 # 7. Примените декораторы для реализации логирования операций по учету студентов.
-
