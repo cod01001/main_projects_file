69a6d63d38f6ed5eb188c4c545ef1b20a06f8c14
Mark <smailsist766@gmail.com>
2023-11-25 11:17:14 -0500

    save 11.25.2023

---
+++ work/10.20.2023.py
@@ -0,0 +1,30 @@
+# Given a list of rows of a square matrix, find the product of the main diagonal.
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# return 45
+#7 kyu
+
+# def list(x):
+#     len_list = len(x)
+#     r = 0
+#     square = 1
+#     for i in x:
+#         square *= i[r]
+#         r += 1
+#     print(square)
+# list([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# easy level
+
+print('Hello')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ work/11.23.2023.py
@@ -0,0 +1,56 @@
+import copy
+
+
+
+# Given a string indicating a range of letters, return a string which includes all the
+# letters in that range, including the last letter. Note that if the range is given
+# in capital letters, return the string in capitals also!
+
+
+# def transpose(matrix):
+#     new_matrix = []
+#     test = []
+#     s = 0
+#     x = len(matrix[0])
+#
+#     while x > 0:
+#         for i in matrix:
+#
+#             test.append(i[s])
+#         new_matrix.append(test.copy())
+#         test.clear()
+#         x -= 1
+#         s += 1
+#     print(new_matrix)
+#
+# transpose([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+
+
+
+# def open_or_senior(data):
+#     x = []
+#     for i in data:
+#         if i[0] >= 55 and i[1] > 7:
+#             x.append('Senior')
+#         else:
+#             x.append('Open')
+#
+#     return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
