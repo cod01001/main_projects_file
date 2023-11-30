61cafef7f89bbb6b2503a5fc6b16da35d1428321
Mark <smailsist766@gmail.com>
2023-11-29 22:17:53 -0500

    save 11.27.2023

---
+++ work/11.29.2023.py
@@ -0,0 +1,19 @@
+
+def noonerize(numbers):
+    #if numbers[0] or numbers[1] is type(str):
+    if False:
+        return 12312123
+    else:
+        numbers = str(numbers[0]), str(numbers[1])
+        x1 = numbers[0][0]+numbers[1][1:]
+        x2 = numbers[1][0] + numbers[0][1:]
+        return x2
+
+
+
+print(noonerize([12345, 90909]))
+
+
+
+
+
